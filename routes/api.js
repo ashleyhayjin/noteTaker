ad15c18f899533dcb5a08d2cf5d38f1228a853e6
Ashley Lee <ashleyhayjin@iMac.fios-router.home>
2021-03-30 21:09:31 -0500

    Server, API Routes

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,8 @@ const router = express.Router();
 let notes = require('../db.json');
 
 router.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, "../db.json"));
+    console.log('Get request received');
+    res.json(notes);
 });
 
 router.get('/api/notes/:id', (req,res) =>{
@@ -14,10 +15,34 @@ router.get('/api/notes/:id', (req,res) =>{
 });
 
 router.post('/api/notes', (req, res) => {
-    fs.writeFile(path.join(__dirname, "../db.json"), JSON.stringify(req.body), function(){     
-        res.send("note saved");
-    })
+    req.body.id = notes.length;
+    console.log(req.body)
     const newNote = req.body;
-
+    notes.push(newNote);
+    console.log(notes);
+    fs.writeFile(path.join(__dirname, "../db.json"), JSON.stringify(notes), (err, res) => {
+        if (err) throw err;
+    })
+    res.send("New Note has been created");
 });
+
+router.delete('/api/notes/:id', (req,res) => {
+    console.log("Delete Works");
+    let index;
+    console.log(req.params.id);
+    for(let i=0; i < notes.length; i++){
+        console.log(notes[i].id === parseInt(req.params.id));
+        if(notes[i].id === parseInt(req.params.id)){
+            index = i;
+        }
+    }
+    console.log("index", index);
+    console.log(notes)
+    console.log(req.params.id);
+    notes.splice(index, 1);
+    console.log(notes);
+    fs.writeFile(path.join(__dirname, "../db.json", JSON.stringify(notes), (err, res) => {
+        if (err) throw err;
+    }))
+})
 module.exports = router;
