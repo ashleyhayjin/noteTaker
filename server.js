06017a46e3d14413e4e52154abfebf4b7d94fd67
Ashley Lee <ashleyhayjinlee@gmail.com>
2021-03-31 19:29:08 -0500

    Heroku

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const fs = require("fs");
 const htmlRoutes = require("./routes/html");
 const apiRoutes = require("./routes/api");
 const app = express();
-const PORT = 3000;
+
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
